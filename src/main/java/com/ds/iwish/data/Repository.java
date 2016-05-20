f26fc436b39f95f317c7b739956bddd83d14de6d
Denys Shevchenko <Denys Shevchenko>
2016-05-21 02:12:17 +0300

    Initial static supply

---
+++ src/main/java/com/ds/iwish/data/Repository.java
@@ -0,0 +1,7 @@
+package com.ds.iwish.data;
+
+/**
+ * Created by Denys_Shevchenko on 4/28/2016.
+ */
+public class Repository {
+}
---
+++ src/main/java/com/ds/iwish/service/WishlistService.java
@@ -0,0 +1,7 @@
+package com.ds.iwish.service;
+
+/**
+ * Created by Denys_Shevchenko on 4/28/2016.
+ */
+public class WishlistService {
+}
